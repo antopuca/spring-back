5d0ae281690bda6e5937f71b0ea4d878b763331c
anthoo <pucaantonia@gmail.com>
2022-09-06 22:31:22 -0300

    Algunos metodos del auto

--- src/main/java/ar/com/codoacodo/Main.java
+++ src/main/java/ar/com/codoacodo/Main.java
@@ -2,22 +2,24 @@ package ar.com.codoacodo;
 
 public class Main {
 
-	//voy a darle play al programa
 	public static void main(String[] args) {
+		
+		//byte < short < int < long < float < double
+		
+		//5 km
+		Auto miPrimerAuto = new Auto(5);//f3 f5		
 
-		System.out.println("hola mundo");
-
-		//voy a crear una instancia de la clase Auto
-
-		Auto miPrimerAuto = new Auto();//nace mi auto
-		//aca ya puedo usar los metodos de ese auto
-
-		miPrimerAuto.mostrarInfo();
-
-		//ejecutar el metodo encender()
 		miPrimerAuto.encender();
+		
+		miPrimerAuto.quintaAFondo();
+		
+		System.out.println("ahora frenando....");
+		
+		//miPrimerAuto.detener();
 
-		miPrimerAuto.mostrarInfo();
+		miPrimerAuto.apagar();
+		
+		System.out.println("fin");
 	}
 
 }
